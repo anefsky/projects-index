969303f254c097e5e593869e3c18243ebbbacd70
Andrew Nefsky <anefsky@gmail.com>
2017-06-19 16:16:19 -0400

    Use ngFor to create table header.

--- src/app/projects-list/projects-list.component.ts
+++ src/app/projects-list/projects-list.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-projects-list',
-  templateUrl: './projects-list.component.html',
-  styleUrls: ['./projects-list.component.scss']
+	selector: 'app-projects-list',
+	templateUrl: './projects-list.component.html',
+	styleUrls: ['./projects-list.component.scss']
 })
 export class ProjectsListComponent implements OnInit {
+	headings: object;
 
-  constructor() { }
+	constructor() { 
+	}
 
-  ngOnInit() {
-  }
+	ngOnInit() {
+		this.headings = [
+		{
+			label: "name"
+		},
+		{
+			label: "source"
+		},
+		{
+			label: "hosted"
+		},
+		{
+			label: "description"
+		}
+		];
+	}
 
 }
