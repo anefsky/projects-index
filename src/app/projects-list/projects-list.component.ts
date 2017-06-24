119f71491627132873d57503ff45bc3153556fc4
Andrew Nefsky <anefsky@gmail.com>
2017-06-24 15:46:01 -0400

    added clock project

--- src/app/projects-list/projects-list.component.ts
+++ src/app/projects-list/projects-list.component.ts
@@ -30,22 +30,28 @@ export class ProjectsListComponent implements OnInit {
 
 		this.listings = [
 		{
+			name: "Clock",
+			source: "https://github.com/anefsky/clock",
+			hosted: "http://andrewnefsky.com/projects/clock",
+			description: "Analogue clock built with just html, css, and javascript.  No frameworks or libraries used."
+		},
+		{
 			name: "Purchasing app",
 			source: "https://github.com/anefsky/penguin",
 			hosted: "http://andrewnefsky.com/projects/penguin",
-			description: "Internal business application use for coordinating pricing between bidders and buyers.  Uses angular, scss, bootstrap."
+			description: "Internal business application used for coordinating pricing between bidders and buyers.  Uses angular, sass, bootstrap."
 		},
 		{
 			name: "Ecommerce app",
 			source: "https://github.com/anefsky/catalogue_page",
 			hosted: "http://andrewnefsky.com/projects/catalogue_page",
-			description: "Prototype for single page ecommerce app.  Uses angular, scss."
+			description: "Prototype for single page ecommerce app.  Uses angular, sass."
 		},
 		{
 			name: "Time sheet header",
 			source: "https://github.com/anefsky/virtusa",
 			hosted: "http://andrewnefsky.com/projects/virtusa",
-			description: "Partial view of timesheet application.  Uses angular, scss, font-awesome."
+			description: "Partial view of timesheet application.  Uses angular, sass, font-awesome."
 		}
 		];
 	}
